75cf6525390c79786ac99ceb1030bcb178df3078
Pyrvdox <cierech8991@gmail.com>
2024-03-27 20:54:30 +0100

    register view
    register

--- backend/api/models.py
+++ backend/api/models.py
@@ -4,3 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     picture = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
