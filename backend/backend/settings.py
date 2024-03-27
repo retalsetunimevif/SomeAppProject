d39926155a2b24cd1e54cfd2c8ab870b9e62c533
Pyrvdox <cierech8991@gmail.com>
2024-03-27 20:25:52 +0100

    migrations
    migrations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#AUTH_USER_MODEL = 'backend.CustomUser'
+AUTH_USER_MODEL = 'api.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
